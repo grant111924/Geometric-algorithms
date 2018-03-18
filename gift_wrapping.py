d0a69c61df91647f3f8c7a9de4caafccc3b61ac5
Grant <grant111924@gmail.com>
2018-03-18 10:44:22 +0800

    version2
     select point 寫一半

--- gift_wrapping.py
+++ gift_wrapping.py
@@ -1,24 +1,36 @@
 #-*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-"""
-class gift_wrapping():
-    def __init__(self):
-        pass
-"""
 
 def generate_data(low,high,size):
     return np.random.randint(low,high,size=size)
-def get_loweset_point(data):
+def get_lowest_point(data):
     lowsetPoint=np.zeros((1,2))
+    start=0
     for index,item in enumerate(data):
         if index == 0:
             lowsetPoint=item
         elif lowsetPoint[1]>=item[1] or (lowsetPoint[1]==item[1] and lowsetPoint[0]<item[0]):
             lowsetPoint=item
-    return lowsetPoint
-def select_point():
+            start=index
+    return lowsetPoint,start
+def CCW(a,b,c):
+    return (b[0]-a[0])*(c[1]-a[1]) - (c[0]-a[0])*(b[1]-a[1])
+def main():
+    data=generate_data(0,20,size=(10,2))
+    lowestPoint,start=get_lowest_point(data)
+    
+    n=len(data)
+    endPoint=lowestPoint
+    path=[]
+    path.append(start)# first point
+    for i in range(1,n):
+	    if (endPoint[0] == pointOnHull[0] and endpoint[1] == pointOnHull[1]) or not CCW(data[i],P[i],endPoint):
+			endPoint = data[i]
+
+
     pass
+
 def plot(data):
     x=[]
     y=[]
@@ -32,10 +44,7 @@ def plot(data):
     plt.show()
 
 if __name__ == "__main__":
-    data=generate_data(0,20,size=(10,2))
-    print(data)
-    x=get_loweset_point(data)
-    print(x)
-    plot(data)
-
-    pass
+    
+    main()
+    #plot(data)
+    
