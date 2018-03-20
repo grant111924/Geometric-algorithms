67eae84d9131a063deef480d9e0e49a3c46395a7
Grant <grant111924@gmail.com>
2018-03-20 16:02:48 +0800

    Finish
    1.gift wrapping 完整算法
    2.plt 畫圖

--- gift_wrapping.py
+++ gift_wrapping.py
@@ -6,45 +6,55 @@ def generate_data(low,high,size):
     return np.random.randint(low,high,size=size)
 def get_lowest_point(data):
     lowsetPoint=np.zeros((1,2))
-    start=0
     for index,item in enumerate(data):
         if index == 0:
             lowsetPoint=item
         elif lowsetPoint[1]>=item[1] or (lowsetPoint[1]==item[1] and lowsetPoint[0]<item[0]):
             lowsetPoint=item
-            start=index
-    return lowsetPoint,start
+    return lowsetPoint
+
 def CCW(a,b,c):
-    return (b[0]-a[0])*(c[1]-a[1]) - (c[0]-a[0])*(b[1]-a[1])
-def main():
-    data=generate_data(0,20,size=(10,2))
-    lowestPoint,start=get_lowest_point(data)
-    
-    n=len(data)
-    endPoint=lowestPoint
-    path=[]
-    path.append(start)# first point
-    for i in range(1,n):
-	    if (endPoint[0] == pointOnHull[0] and endpoint[1] == pointOnHull[1]) or not CCW(data[i],P[i],endPoint):
-			endPoint = data[i]
+    if (b[0]-a[0])*(c[1]-a[1]) >= (c[0]-a[0])*(b[1]-a[1]) :
+        return True
+    return False
 
+def gift_wrapping():
+    S=generate_data(0,20,size=(10,2))
+    lowestPoint=get_lowest_point(S) #　 search lowest points
+    pointOnHull=lowestPoint  #  lowest and leftmost point  in S 
+    n=len(S) #S total count
+    i=0 
+    print(S)
+    path = [None]*n   
+    while True:
+        #print(path[i])
+        path[i]=pointOnHull
+        endPoint=S[0]
+        for j in range(1,n):
+            if (endPoint[0] == pointOnHull[0] and endPoint[1] == pointOnHull[1]) or not CCW(S[j],path[i],endPoint):
+                endPoint = S[j]
+        i=i+1
+        pointOnHull = endPoint
+        if endPoint[0] == path[0][0] and endPoint[1] == path[0][1]:
+            break
+     
 
-    pass
+    while path[-1] is None :
+        path.pop(i)
+    print(path) 
+    path=np.array(path)
+    return path,S
+def plot(L,P):
 
-def plot(data):
-    x=[]
-    y=[]
-    for item  in data:
-        x.append(item[0])
-        y.append(item[1])
-    print(x)
-    print(y)
-    plt.plot(x,y,'ro')
-    plt.axis([0, 20, 0, 20])
+    plt.figure()
+    plt.plot(L[:,0],L[:,1], 'b-', picker=5)
+    plt.plot([L[-1,0],L[0,0]],[L[-1,1],L[0,1]], 'b-', picker=5)
+    plt.plot(P[:,0],P[:,1],".r")
+    plt.axis('off')
     plt.show()
-
+    
 if __name__ == "__main__":
     
-    main()
-    #plot(data)
+    data,total=gift_wrapping()
+    plot(data,total)
     
